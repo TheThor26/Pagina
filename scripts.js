b5791b694d586d52d0e3fef8dfd54d9b2821eb16
TheThor26 <66410751+TheThor26@users.noreply.github.com>
2020-06-11 22:54:21 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*!
+    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
+    */
+ $(function () {
+  $('.example-popover').popover({
+    container: 'body'
+  })
+})
